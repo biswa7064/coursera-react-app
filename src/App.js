429134f3ac8ce1c7db17c330c33e202babd8f634
biswa7064 <biswaranjan16856@gmail.com>
2020-09-20 19:15:24 +0530

    navbar added using reactstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+
+//adding navigation bar
+import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar  color = "dark">
+        <div className = "container">
+          <NavbarBrand href = "https://github.com/biswa7064/coursera-react-app">Welcome to my github account</NavbarBrand>
+        </div>
+      </Navbar>
+      
     </div>
   );
 }
