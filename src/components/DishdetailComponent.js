1726899ad4a23c862590f716c0fc398366aadd42
biswa7064 <biswaranjan16856@gmail.com>
2020-09-29 04:06:35 +0530

    Single Page Application part-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
  
 
-import  {Card,CardImg,CardBody,CardText,CardTitle} from "reactstrap";
+import  {Card,CardImg,CardBody,CardText,CardTitle,Breadcrumb, BreadcrumbItem} from "reactstrap";
+import { Link } from 'react-router-dom';
 
 
     
@@ -75,16 +76,28 @@ import  {Card,CardImg,CardBody,CardText,CardTitle} from "reactstrap";
   const DishDetail = (props)=>{
       if(props.dish != null){
       return(
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
           
          <div className = "row">
               <div className = "col-12 col-md-5 lm-1">
               <RenderDish dish ={props.dish} />
               </div>          
             <div className = "col-12 col-md-5 lm-1">                         
-            <RenderComments comments = {props.dish.comments}/>
+            <RenderComments comments = {props.comments}/>
             </div>
-          
             </div>
+        </div>
           
       )
       }else{
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,23 @@
 import React, {Component} from "react";
-import  {Card,CardImg,CardImgOverlay,CardTitle} from "reactstrap";
+import  {Card,CardImg,CardImgOverlay,CardTitle,Breadcrumb,BreadcrumbItem} from "reactstrap";
 
 //import DishDetail from "./DishdetailComponent";
+import {Link} from "react-router-dom";
 
 function RenderMenuItem({dish}){
     return(
         
             
-            <Card >                       
-                <CardImg width = "100%" src = {dish.image} alt = {dish.name}/>
+            <Card > 
+                <Link to = {`menu/${dish.id}`}>                      
+                    <CardImg width = "100%" src = {dish.image} alt = {dish.name}/>
                     
                     <CardImgOverlay>
                         <CardTitle style = {{color:"darkslateblue", fontWeight:"bold", fontSize:"30px"}}>
                         {dish.name}
                         </CardTitle>                           
                     </CardImgOverlay>
+                </Link>
             </Card>
 
             
@@ -40,10 +43,18 @@ const Menu = (props)=>{
         
         <div className = "container">                
             <div className = "row">
-                
-                {menu} 
-                
-            </div>              
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                <BreadcrumbItem active>Menu</BreadcrumbItem>
+            </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>                
+            </div>
+                <div className="row">
+                    {menu}
+                </div>              
         </div>
         
     );
