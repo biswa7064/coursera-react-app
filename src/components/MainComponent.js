7a56fdb92178170e2f39b75bd96acb8d6102a907
biswa7064 <biswaranjan16856@gmail.com>
2020-09-29 23:38:11 +0530

    AboutUs page added[Assignment-2]

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,10 +9,12 @@ import Header from "./HeaderComponent.js";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import {Switch, Route, Redirect} from "react-router-dom";
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+
   
 
 class Main extends Component {
@@ -58,6 +60,7 @@ class Main extends Component {
         <Route exact path = "/menu" component = {()=><Menu dishes = {this.state.dishes}/>}/>
         <Route path='/menu/:dishId' component={DishWithId} />
         <Route exact path = "/contactus" component = {Contact}/>
+        <Route exact path = "/aboutus" component = {()=><About leaders = {this.state.leaders}/>}/>
         <Redirect to = "/home"/>
       </Switch>
       <Footer/>
