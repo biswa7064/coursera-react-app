28b6c025cf07662bd5d8784e4871eeb90ef1c1f0
biswa7064 <biswaranjan16856@gmail.com>
2021-01-02 15:28:47 +0530

    Updates

---
+++ precache-manifest.4c8768cb80983905338ecb7011787656.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0eaac90b0c66365bed9a7cc263c751",
+    "url": "/coursera-react-app/index.html"
+  },
+  {
+    "revision": "cbf23fbcacd52cf6b79d",
+    "url": "/coursera-react-app/static/css/2.283353eb.chunk.css"
+  },
+  {
+    "revision": "f2aac80bcdcf8cb85062",
+    "url": "/coursera-react-app/static/css/main.cca4ba05.chunk.css"
+  },
+  {
+    "revision": "cbf23fbcacd52cf6b79d",
+    "url": "/coursera-react-app/static/js/2.df172797.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coursera-react-app/static/js/2.df172797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2aac80bcdcf8cb85062",
+    "url": "/coursera-react-app/static/js/main.b52ddfc9.chunk.js"
+  },
+  {
+    "revision": "6222df9d364996340c44",
+    "url": "/coursera-react-app/static/js/runtime-main.a5870c84.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coursera-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coursera-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coursera-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coursera-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coursera-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
